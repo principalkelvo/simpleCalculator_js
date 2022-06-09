9ab66765407b408c39990d7f2bf7a32181a8bdab
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-06-09 23:14:14 +0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+//calculator
+function add(a,b){
+    return a+b;
+}
+function subtract(a,b){
+    return a-b;
+}
+function multiply(a,b){
+    return a*b;
+}
+function divide(a,b){
+    return a/b;
+}
+function operate(operator,a,b){
+    switch(operator){
+        case '+':
+            return add(a,b);
+        case '-':
+            return subtract(a,b);
+        case '*':
+            return multiply(a,b);
+        case '/':
+            return divide(a,b);
+    }
+}
+function calculate(operator,a,b){
+    return operate(operator,a,b);
+}
+console.log(calculate('+',1,2));
